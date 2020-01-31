cff338b1bacdc86b4d3d04349d4a2d9e182dba26
Nicolas Modrzyk <hellonico@gmail.com>
2020-01-31 13:43:58 +0900

    update samples

--- project.clj
+++ project.clj
@@ -1,26 +1,30 @@
 (defproject origami-simple "0.1-SNAPSHOT"
 :test-paths ["test"]
-:plugins [[hellonico/lein-gorilla "0.4.2"][lein-auto "0.1.3"][lein-jupyter "0.1.16"]]
+:plugins [
+          ; [hellonico/lein-gorilla "0.4.2"] 
+          [lein-jupyter "0.1.17"]
+          ]
 :auto {:default {:file-pattern #"\.(clj)$"}}
 :main opencv4.ok
 :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
-:aliases {"notebook" ["gorilla" ":ip" "0.0.0.0" ":port" "10000"]}
+; :aliases {"notebook" ["gorilla" ":ip" "0.0.0.0" ":port" "10000"]}
 :profiles {:dev {
   :resource-paths ["resources"]
   :dependencies [
   ; used for proto repl
-  [org.clojure/tools.nrepl "0.2.11"]
+  ; [org.clojure/tools.nrepl "0.2.11"]
   ; proto repl
-  [proto-repl "0.3.1"]
+  ; [proto-repl "0.3.1"]
   ; use to start a gorilla repl
-  [hellonico/gorilla-repl "0.4.1"]
-  [seesaw "1.4.5"]]
+  [clojupyter "0.3.0"] 
+  ; [hellonico/gorilla-repl "0.4.1"]
+  [seesaw "1.5.0"]]
   }}
 :dependencies [
- [org.clojure/clojure "1.9.0"]
- [org.clojure/tools.cli "0.3.5"]
+ [org.clojure/clojure "1.10.1"]
+ [org.clojure/tools.cli "0.4.2"]
 
- [origami "4.2.0-0"]
+ [origami "4.2.0-1-SNAPSHOT"]
 
  ; uncomment to use only the binary for your platform
  ; [origami "4.2.0-0" :exclusions [opencv/opencv-native]]
